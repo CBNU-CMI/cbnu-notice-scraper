572adee3b3722f1c9150aea7c22044582b3f895a
jaryapp <jaryapp@gmail.com>
2020-04-11 17:35:41 +0900

    소프트웨어학과 공지사항 크롤링

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const puppeteer = require('puppeteer');
     });
     const page = await browser.newPage();
     await page.goto('https://software.cbnu.ac.kr/bbs/bbs.php?db=notice&pgID=ID12415888101');
-    await page.screenshot({
-        path: 'test.png'
-    });
+
+    var data = await page.evaluate(() => {
+        getData: function getData() {
+            var list = document.querySelectorAll('#content > table:nth-child(8) > tbody > tr')
+            let i = 3;
+            let data = []
+            while (i < list.length) {
+                let td = list[i].querySelectorAll('td')
+                data.push({
+                    site: "소프트웨어학과",
+                    category: "공지사항",
+                    site_id: 130101,
+                    title: td[2].innerText.trim(),
+                    url: td[2].querySelector('a').href.trim(),
+                    date: td[6].innerText.trim()
+                })
+                i++
+            }
+            return data
+        }
+        return getData()
+    })
+
+    console.log(data)
 
     await browser.close();
 })();
